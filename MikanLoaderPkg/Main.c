a93819ee61b20312c63b1f48e8220e7aa2058721
kudojp <heyjudejudejude1968@gmail.com>
2022-02-11 15:50:35 +0900

    Add #GetMemoryMap in MikanLoaderPkg/Main.c

--- MikanLoaderPkg/Main.c
+++ MikanLoaderPkg/Main.c
@@ -8,3 +8,30 @@ EFI_STATUS EFIAPI UefiMain(
   while (1);
   return EFI_SUCCESS;
 }
+
+// #@@range_begin(struct_memory_map)
+struct MemoryMap {
+  UINTN buffer_size;
+  VOID* buffer;
+  UINTN map_size;
+  UINTN map_key;
+  UINTN descriptor_size;
+  UINT32 descriptor_version;
+};
+// #@@range_end(struct_memory_map)
+
+// #@@range_begin(get_memory_map)
+EFI_STATUS GetMemoryMap(struct MemoryMap* map) {
+  if (map->buffer == NULL) {
+    return EFI_BUFFER_TOO_SMALL;
+  }
+
+  map->map_size = map->buffer_size;
+  return gBS->GetMemoryMap(
+      &map->map_size,
+      (EFI_MEMORY_DESCRIPTOR*)map->buffer,
+      &map->map_key,
+      &map->descriptor_size,
+      &map->descriptor_version);
+}
+// #@@range_end(get_memory_map)
